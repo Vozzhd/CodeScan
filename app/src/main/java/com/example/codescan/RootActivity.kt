8606915491dde5314428a27cc5fc4f5c991927bb
Vozzhd <Vozzhd@yandex.ru>
2024-10-21 14:05:03 +0300

    Добавлены: -navigation_graph -bottom_navigation -binding -containerView

---
+++ app/src/main/java/com/example/codescan/RootActivity.kt
@@ -0,0 +1,29 @@
+package com.example.codescan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.codescan.databinding.ActivityRootBinding
+
+class RootActivity : AppCompatActivity() {
+
+    private var _binding: ActivityRootBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityRootBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = binding.bottomNavigationView
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
